9134e5f969f40cab0b5bdeb01f5de7e96a90a9fc
pivasblazer <pivasblazer@mail.ru>
2023-05-08 22:46:22 +0300

    Добавьте файлы проекта.

---
+++ LobovDiplom/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LobovDiplom
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Показываем окно авторизации при запуске приложения
+            var loginPage = new Auth();
+            loginPage.ShowDialog();
+            base.OnStartup(e);
+        }
+    }
+}
